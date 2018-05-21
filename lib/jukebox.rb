05303486ad17af9ed202d156f94b30d87a56824b
Matt Maher <mattmaher0@gmail.com>
2018-05-21 15:25:50 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -14,5 +14,27 @@ def help
   puts "I accept the following commands:
   - help: displays this help message
   - list: displays a list of songs you can play
-  - play: "
+  - play: lets you choose a song to play
+  - exit: exits this program"
 end  
+
+def list(songs)
+  songs.each_with_index |song, idx|
+  puts "#{idx + 1}. #{song}"
+end  
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  input = gets.chomp
+  if songs.include?(input) || 0 < input < 10
+    puts "Playing <song name>"
+  else 
+    puts "Invalid input, please try again"
+end  
+
+def exit_jukebox
+  puts "Goodbye"
+end  
+
+
+  
